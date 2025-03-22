3666a5354828cb877bd372f6610056ed18783c61
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-03-22 15:25:15 -0400

    Testing Terraform Deployment-2

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,34 +33,6 @@ resource "google_compute_firewall" "airflow_firewall" {
 
 
 
-resource "google_compute_region_instance_group_manager" "airflow_mig" {
-  name                = "airflow-mig"
-  base_instance_name  = "airflow-instance"
-  region              = var.region
-  project             = var.project_id
-  version {
-    instance_template = google_compute_instance_template.airflow_template.self_link
-  }
-  target_size = 1  # minimum number of instances
-
-  # Optional auto-scaling block
-  auto_healing_policies {
-    health_check      = google_compute_health_check.http_health_check.self_link
-    initial_delay_sec = 300
-  }
-}
-
-# Example Health Check
-resource "google_compute_health_check" "http_health_check" {
-  name               = "airflow-health-check"
-  project            = var.project_id
-  check_interval_sec = 10
-  timeout_sec        = 5
-  http_health_check {
-    port = 8080
-  }
-}
-
 resource "google_compute_autoscaler" "airflow_autoscaler" {
   name         = "airflow-autoscaler"
   project      = var.project_id
