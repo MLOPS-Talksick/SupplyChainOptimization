d435556787f20640bc288fc2d5261f4f19c162e8
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-03-23 01:59:53 -0400

    Testing Terraform Deployment-43

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -43,7 +43,7 @@ variable "custom_image_name" {
 }
 
 variable "gcp_service_account_key" {
-  type      = json
+  type      = string
   sensitive = true
   description = "GCP Service Account key in JSON format"
 }
