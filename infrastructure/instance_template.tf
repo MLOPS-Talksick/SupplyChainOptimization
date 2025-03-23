9f2da391db1b276bbf514d0c262a3680927a8e1c
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-03-22 20:21:40 -0400

    Testing Terraform Deployment-26

--- infrastructure/instance_template.tf
+++ infrastructure/instance_template.tf
@@ -39,7 +39,7 @@ resource "google_compute_instance_template" "airflow_template" {
           echo "Adding Docker repository..."
           sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
           curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-          sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable"
+          sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $$ (lsb_release -cs) stable"
           sudo apt-get update -y
           sudo apt-get install -y docker-ce docker-ce-cli containerd.io
       fi
