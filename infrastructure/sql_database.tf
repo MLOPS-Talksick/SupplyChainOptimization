3e32d5f72c32c142993832c82c93194f346f71dc
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-03-26 20:12:00 -0400

    Added internal network configurations-7

--- infrastructure/sql_database.tf
+++ infrastructure/sql_database.tf
@@ -7,8 +7,11 @@ resource "google_sql_database_instance" "instance" {
   settings {
     tier = "db-f1-micro"
     ip_configuration {
-      ipv4_enabled = false
-      private_network = google_compute_network.airflow_vpc.self_link
+      ipv4_enabled = true
+      authorized_networks {
+        name  = "allowed-network"
+        value = "0.0.0.0/0"  # e.g., "203.0.113.45/32" or "0.0.0.0/0" (for demo only)
+      }
     }
   }
 }
