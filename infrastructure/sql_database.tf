aa00daaec25613bc1475c0e5e8904850da60a857
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-03-26 20:04:09 -0400

    Added internal network configurations-6

--- infrastructure/sql_database.tf
+++ infrastructure/sql_database.tf
@@ -1,41 +1,14 @@
-resource "google_compute_network" "private_network" {
-  provider = google-beta
-  name     = "private-network"
-}
-
-resource "google_compute_global_address" "private_ip_address" {
-  provider     = google-beta
-  name         = "private-ip-address"
-  purpose      = "VPC_PEERING"
-  address_type = "INTERNAL"
-  prefix_length = 16
-  network      = google_compute_network.private_network.id
-}
-
-resource "google_service_networking_connection" "private_vpc_connection" {
-  provider = google-beta
-  network  = google_compute_network.private_network.id
-  service  = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-}
-
-
 resource "google_sql_database_instance" "instance" {
   provider         = google-beta
-  name             = "transaction-database"
+  name             = "public-instance-${random_id.db_name_suffix.hex}"
   region           = "us-central1"
   database_version = "MYSQL_5_7"
 
-  depends_on = [google_service_networking_connection.private_vpc_connection]
-
   settings {
     tier = "db-f1-micro"
-
     ip_configuration {
-      ipv4_enabled                                  = false
-      private_network                               = google_compute_network.private_network.self_link
-      allocated_ip_range                            = google_compute_global_address.private_ip_address.name
-      enable_private_path_for_google_cloud_services = true
+      ipv4_enabled = false
+      private_network = google_compute_network.airflow_vpc.self_link
     }
   }
 }
