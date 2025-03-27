2189b2238f6d8e37bb6eb2374f737c287391a64a
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-03-27 01:37:29 -0400

    Testing SQL Terraform with private network configurations-2

--- infrastructure/sql_database.tf
+++ infrastructure/sql_database.tf
@@ -11,6 +11,12 @@ resource "google_sql_database_instance" "instance" {
       private_network = google_compute_network.my_network.self_link
     }
   }
+
+  depends_on = [
+    google_service_networking_connection.private_vpc_connection,
+    google_compute_global_address.private_ip_range,
+    google_compute_subnetwork.my_subnet,
+  ]
 }
 
 
