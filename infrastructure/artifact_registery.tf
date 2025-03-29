62773f2ceb910cece0ba439b3294b58e96900a92
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-03-29 02:34:54 -0400

    Testing artifact registery setup-6

--- infrastructure/artifact_registery.tf
+++ infrastructure/artifact_registery.tf
@@ -1,7 +1,7 @@
 resource "google_artifact_registry_repository" "airflow_docker_repo" {
   repository_id     = var.artifact_registry_name  # e.g., "airflow-docker-image"
   project           = var.project_id
-  location          = "us"
+  location          = "us-central1"
   format            = var.repo_format
   description       = "Docker repository for data-pipeline"
 }
