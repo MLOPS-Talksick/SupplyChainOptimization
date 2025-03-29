cd5d0ca5e564ebfc7142147e7d71553808232dc2
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-03-28 23:36:57 -0400

    Testing Bucket Creation-2

--- infrastructure/sql_database.tf
+++ infrastructure/sql_database.tf
@@ -12,6 +12,10 @@ resource "google_sql_database_instance" "instance" {
     }
   }
 
+  lifecycle {
+    ignore_changes = [deletion_protection]
+  }
+
   depends_on = [
     google_service_networking_connection.private_vpc_connection,
     google_compute_global_address.private_ip_range,
