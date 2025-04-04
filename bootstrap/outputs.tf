cc2cc73e0dd8164b3b79021c91d5bea0172b0f40
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-04-04 01:53:22 -0400

    Testing service account configurations with terraform-53

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,5 +1,5 @@
 output "terraform_sa_private_key" {
   # google_service_account_key.my_sa_key.private_key is base64
-  value     = base64decode(google_service_account_key.my_sa_key.private_key)
+  value     = base64decode(google_service_account_key.terraform_sa_key.private_key)
   sensitive = true
 }
