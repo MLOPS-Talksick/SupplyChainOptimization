9a8925accb1be58c89ec3447e60d9dd5ba04ed55
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-04-03 22:44:26 -0400

    Testing service account configurations with terraform-22

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -3,8 +3,3 @@ output "terraform_sa_private_key" {
   value       = google_service_account_key.terraform_sa_key.private_key
   sensitive   = true
 }
-
-output "terraform_sa_email" {
-  description = "The email address of the Terraform service account"
-  value       = google_service_account.terraform_sa.email
-}
