365db61c2e219b3f21e278e083a7704843403236
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-04-04 03:03:00 -0400

    Testing service account configurations with terraform-60

--- bootstrap/providers.tf
+++ bootstrap/providers.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"  # or latest stable
+    }
+  }
+}
+
 provider "google" {
   credentials = var.bootstrap_gcp_key
   project     = var.project_id
