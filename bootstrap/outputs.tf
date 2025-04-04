beda8d17e7ca542c4039670c1a8917826e36f07f
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-04-04 02:09:55 -0400

    Testing service account configurations with terraform-55

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,5 +1,5 @@
 output "terraform_sa_private_key" {
   description = "Private key (in JSON format) for the bootstrap service account"
   value       = google_service_account_key.terraform_sa_key.private_key
-  sensitive   = true
+  # sensitive   = true
 }
