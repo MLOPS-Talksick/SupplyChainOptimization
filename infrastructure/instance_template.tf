3c77ee84c7a686dc0eff33d0bae3f2af809bd086
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-04-04 21:03:30 -0400

    Testing service account updated deploy configurations-12

--- infrastructure/instance_template.tf
+++ infrastructure/instance_template.tf
@@ -85,8 +85,9 @@ resource "google_compute_instance_template" "airflow_template" {
       fi
       echo "Creating GCP Key File..."
             cat > /opt/airflow/gcp-key.json <<EOKEY
-      ${jsonencode(var.gcp_service_account_key)}
-      EOKEY
+            ${var.gcp_service_account_key}
+            EOKEY
+
       
       chmod 644 /opt/airflow/gcp-key.json
       sudo chown ubuntu:docker /opt/airflow/gcp-key.json
