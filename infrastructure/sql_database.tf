dacb492b1169a57e6515396b6b3d85435b281675
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-04-10 01:00:32 -0400

    Testing load-balancer deployment with backend-15

--- infrastructure/sql_database.tf
+++ infrastructure/sql_database.tf
@@ -1,39 +1,39 @@
-# # Create the Cloud SQL instance
-# resource "google_sql_database_instance" "instance" {
-#   provider         = google-beta
-#   name             = "mlops-sql-2"
-#   region           = var.region
-#   database_version = "MYSQL_8_0"
+# Create the Cloud SQL instance
+resource "google_sql_database_instance" "instance" {
+  provider         = google-beta
+  name             = "mlops-sql-2"
+  region           = var.region
+  database_version = "MYSQL_8_0"
 
-#   settings {
-#     tier = "db-f1-micro"
+  settings {
+    tier = "db-f1-micro"
 
-#     ip_configuration {
-#       ipv4_enabled    = false
-#       private_network = google_compute_network.my_network.self_link
-#     }
-#   }
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.my_network.self_link
+    }
+  }
 
-#   lifecycle {
-#     ignore_changes = [deletion_protection]
-#   }
+  lifecycle {
+    ignore_changes = [deletion_protection]
+  }
 
-#   depends_on = [
-#     google_service_networking_connection.private_vpc_connection,
-#     google_compute_global_address.private_ip_range,
-#     google_compute_subnetwork.my_subnet,
-#   ]
-# }
+  depends_on = [
+    google_service_networking_connection.private_vpc_connection,
+    google_compute_global_address.private_ip_range,
+    google_compute_subnetwork.my_subnet,
+  ]
+}
 
-# # Create the database in the instance
-# resource "google_sql_database" "database" {
-#   name     = var.mysql_database
-#   instance = google_sql_database_instance.instance.name
-# }
+# Create the database in the instance
+resource "google_sql_database" "database" {
+  name     = var.mysql_database
+  instance = google_sql_database_instance.instance.name
+}
 
-# # Create the SQL user
-# resource "google_sql_user" "user" {
-#   name     = var.mysql_user
-#   instance = google_sql_database_instance.instance.name
-#   password = var.mysql_password
-# }
+# Create the SQL user
+resource "google_sql_user" "user" {
+  name     = var.mysql_user
+  instance = google_sql_database_instance.instance.name
+  password = var.mysql_password
+}
