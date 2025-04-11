6a8a9720aeee2e828f4ca9a53ae8f734c6787c14
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-04-11 03:00:37 -0400

    Testing load-balancer deployment with backend-29

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -72,6 +72,8 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_range.name]
 
+  update_on_creation_fail = true
+  
   lifecycle {
     ignore_changes = [reserved_peering_ranges]
   }
