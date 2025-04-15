a4ccd6213c94af143f7b34b42899cb83d89c3fc6
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2025-04-15 19:51:24 -0400

    Testing load-balancer deployment with backend-52

--- infrastructure/gcs_buckets.tf
+++ infrastructure/gcs_buckets.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "buckets" {
   for_each = var.bucket_names
   name     = each.value
-  location = "US"
+  location = var.region
   project  = var.project_id
 
   # Set force_destroy if needed:
