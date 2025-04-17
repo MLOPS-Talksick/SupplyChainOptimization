0e48a935015c9c2d35370c11fcfd15d7c84414dd
Arj Bhat <bhat.ar@northeastern.edu>
2025-04-17 19:20:58 -0400

    fixed one thing

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `http://34.8.2.47/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
