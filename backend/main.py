d97ff6fbadd9a756c503f25c0046c545f41b1984
Shrey V Desai <84085517+ShreyVDesai@users.noreply.github.com>
2025-04-19 14:06:42 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -572,7 +572,7 @@ async def validate_excel(file: UploadFile = File(...)):
         db_query = "SELECT DISTINCT product_name FROM SALES"
         with pool.connect() as conn:
             result = conn.execute(sqlalchemy.text(db_query))
-            db_products = {row[0] for row in result}
+            db_products = set([row[0] for row in result])
         logging.info(f"Fetched {len(db_products)} unique product names from database.")
     except Exception as e:
         logging.error(f"Database query in validate_excel failed: {e}")
