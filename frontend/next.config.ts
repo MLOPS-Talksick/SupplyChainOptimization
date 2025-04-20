7fc233f8e4f14c06d79d6ea15ecae154d794ffae
Arj Bhat <bhat.ar@northeastern.edu>
2025-04-19 20:37:53 -0400

    Skip eslint

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  eslint: {
+    // WARNING: this will allow any lint errors to slip through your build
+    ignoreDuringBuilds: true,
+  },
   async rewrites() {
     return [
       {
